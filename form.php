4ffd837c99edfb6728ce924d14c0b2396e9d2303
Peter Gessler <Pngessler@ku.edu>
2022-04-24 17:53:35 -0500

    Added some data to Tables and started calling queries to them

--- form.php
+++ form.php
@@ -3,7 +3,6 @@
     $class_type = $_POST["class_type"];
     $class_term = $_POST["class_term"];
     $class_instruction_type = $_POST["class_instruction_type"];
-  /*Tables are : CLASS, PROFESSOR, TEACHES, BUILDING, ROOM, HASROOM*/
   
     echo "class code: " . $class_code . "<br>";
     echo "class type: " . $class_type . "<br>";
@@ -15,37 +14,33 @@
     if($mysqli->connect_error){
       die("Connection failed: " . $mysqli->connect_error);
     }
-    /* the dummy table is a dummy table used to get simple queries setup*/
     if($class_code == "" && $class_instruction_type == "Any Instruction Mode"){
-      $query = "SELECT * FROM dummy WHERE ClassTerm='$class_term'
-                AND ClassType='$class_type'";
+      $query = "SELECT ClassCode, PROFESSOR.name, BuildingName, RoomNumber, CreditHours, SeatsAvailable, Time, Mode FROM CLASS, TEACHES, PROFESSOR WHERE CLASS.ClassNumber = TEACHES.ClassNumber
+                AND PROFESSOR.PID = TEACHES.PID";
     }
     else if($class_code != "" && $class_instruction_type == "Any Instruction Mode"){
-      $query = "SELECT * FROM dummy WHERE ClassCode='$class_code'
-                AND ClassTerm='$class_term'
-                AND ClassType='$class_type'";
+      $query = "SELECT ClassCode, PROFESSOR.name, BuildingName, RoomNumber, CreditHours, SeatsAvailable, Time, Mode FROM CLASS, TEACHES, PROFESSOR WHERE ClassCode LIKE '%$class_code%'
+                AND CLASS.ClassNumber = TEACHES.ClassNumber
+                AND PROFESSOR.PID = TEACHES.PID";
     }
     else if($class_code == "" && $class_instruction_type != "Any Instruction Mode"){
-      $query = "SELECT * FROM dummy WHERE ClassIntructionType='$class_instruction_type'
-                AND ClassTerm='$class_term'
-                AND ClassType='$class_type'";
+      $query = "SELECT ClassCode, PROFESSOR.name, BuildingName, RoomNumber, CreditHours, SeatsAvailable, Time, Mode FROM CLASS, TEACHES, PROFESSOR WHERE CLASS.Mode='$class_instruction_type'
+                AND CLASS.ClassNumber = TEACHES.ClassNumber
+                AND PROFESSOR.PID = TEACHES.PID";
     }
     else{
-      $query = "SELECT * FROM dummy WHERE ClassIntructionType='$class_instruction_type'
-                AND ClassTerm='$class_term'
-                AND ClassCode='$class_code'
-                AND ClassIntructionType='$class_instruction_type'";
+      $query = "SELECT ClassCode, PROFESSOR.name, BuildingName, RoomNumber, CreditHours, SeatsAvailable, Time, Mode FROM CLASS, TEACHES, PROFESSOR WHERE CLASS.Mode='$class_instruction_type'
+                AND ClassCode LIKE '%$class_code%'
+                AND CLASS.ClassNumber = TEACHES.ClassNumber
+                AND PROFESSOR.PID = TEACHES.PID";
     }
     $result = $mysqli->query($query);
-
     echo '<h2> Classes: </h2>';
     echo "<table border='1'>";
-    echo "<tr><th>ClassCode</th><th>ClassType</th><th>ClassTerm</th><th>ClassInstructionType</th></tr>";
+    echo "<tr><th>ClassCode</th><th>Professor</th><th>Room Number</th><th>Building Number</th><th>Credit Hours</th><th>Seats Availiable</th><th>Time</th></tr>";
     while($row = mysqli_fetch_array($result)){
-      /*will need to change the array indexes to the attributes of the actual tables*/
-      echo "<tr><td>" . $row['ClassCode'] . "</td><td>" . $row['ClassType'] . "</td><td>" . $row['ClassTerm'] . "</td><td>" . $row['ClassIntructionType'] . "</td></tr>";
-    }
-
+      echo "<tr><td>" . $row['ClassCode'] ."</td><td>". $row['name'] . "</td><td>" . $row['Mode']. "</td><td>" . $row['BuildingName'] . "</td><td>" . $row['RoomNumber'] . "</td><td>" . $row['CreditHours'] . "</td><td>" . $row['SeatsAvailable'] . "</td><td>" . $row['Time'] . "</td></tr>";
+     }
     echo "</table>";
     $mysqli->close();
 
